ebde3e742745b2b2882885ef12cca98d5163ab93
WOOJIN SON <36991763+swj9707@users.noreply.github.com>
2022-07-28 14:54:04 +0900

    Initial commit

---
+++ src/test/java/com/example/bbs_demo/BbsDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.bbs_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootTest
+@EnableJpaAuditing
+class BbsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

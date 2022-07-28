e43c295d7dd35e18eb8448f8a9470ba35933b15f
WOOJIN SON <36991763+swj9707@users.noreply.github.com>
2022-07-28 17:53:08 +0900

    feature/crud_r read data

--- src/test/java/com/example/bbs_demo/service/BoardServiceImplTest.java
+++ src/test/java/com/example/bbs_demo/service/BoardServiceImplTest.java
@@ -27,4 +27,24 @@ class BoardServiceImplTest {
         Long bno = boardService.register(dto);
     }
 
+    @Test
+    public void testList(){
+        PageRequestDTO pageRequestDTO = new PageRequestDTO();
+
+        PageResultDTO<BoardDTO, Object[]> result = boardService.getList(pageRequestDTO);
+
+        for(BoardDTO boardDTO : result.getDtoList()){
+            System.out.println(boardDTO);
+        }
+    }
+
+    @Test
+    public void testGet(){
+        Long bno = 100L;
+
+        BoardDTO boardDTO = boardService.get(bno);
+
+        System.out.println(boardDTO);
+    }
+
 }

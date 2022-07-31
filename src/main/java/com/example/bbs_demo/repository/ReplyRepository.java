56676ede92796aece4ce714cc09dafa883d7ff29
WOOJIN SON <36991763+swj9707@users.noreply.github.com>
2022-07-31 17:06:24 +0900

    feature/reply Reply Entity, View, Service, Controller Setup

--- src/main/java/com/example/bbs_demo/repository/ReplyRepository.java
+++ src/main/java/com/example/bbs_demo/repository/ReplyRepository.java
@@ -1,12 +1,17 @@
 package com.example.bbs_demo.repository;
 
+import com.example.bbs_demo.entity.Board;
 import com.example.bbs_demo.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
     @Modifying
     @Query("delete from Reply r where r.board.bno = :bno")
     void deleteByBno(Long bno);
+
+    List<Reply> getRepliesByBoardOrderByRno(Board board);
 }
